16210bbeb81277dbed28e895aa690397465ec039
Tuomo Venäläinen <tuomoster@gmail.com>
2016-12-01 16:58:33 +0200

    Fixed AT&T-conversion of stage 1 to boot

--- qemu.sh
+++ qemu.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+#DEBUGFLAGS="-s -S"
+DEBUGFLAGS=""
+
 make &&
-qemu-system-i386 \
+qemu-system-i386 $DEBUGFLAGS\
     -drive file=bootdisk.img,format=raw,index=0,if=floppy \
     -boot order=a
